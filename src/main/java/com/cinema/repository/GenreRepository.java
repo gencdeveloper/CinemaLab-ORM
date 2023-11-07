160037ef39833bd2e04f5309f579ed3cc47bf131
Emre <yunusozudogru34@gmail.com>
2023-11-07 22:55:22 +0100

    updated query description

--- src/main/java/com/cinema/repository/GenreRepository.java
+++ src/main/java/com/cinema/repository/GenreRepository.java
@@ -4,4 +4,11 @@ import com.cinema.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GenreRepository extends JpaRepository<Genre,Long> {
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query that return all genres
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query that returns genres by containing name
 }
