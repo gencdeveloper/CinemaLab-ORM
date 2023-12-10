71191cd7c6d73f6029710858530086f82ee2024b
Emre <yunusozudogru34@gmail.com>
2023-12-10 18:59:57 +0100

    Genre querry completed.

--- src/main/java/com/cinema/repository/GenreRepository.java
+++ src/main/java/com/cinema/repository/GenreRepository.java
@@ -2,13 +2,21 @@ package com.cinema.repository;
 
 import com.cinema.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GenreRepository extends JpaRepository<Genre,Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that return all genres
+    @Query("select g from Genre g")
+    List<Genre> fetchAll();
+
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns genres by containing name
+    @Query(value = "SELECT * from Genre where name ilike concat('%',?1,'%') ",nativeQuery = true)
+    List<Genre> retrieveByName(String name);
 }
